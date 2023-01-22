bc9a71b72dd5aa6e04cfb626a86a40ae7e46c01e
dahliasan <laninaesloco@gmail.com>
2023-01-22 22:33:38 +1100

    added semantic search function

--- popup.js
+++ popup.js
@@ -50,74 +50,159 @@ checkForKey().then((response) => {
   }
 })
 
+// Search summaries
+const initSearchFunction = () => {
+  const searchInput = document.getElementById('search_input')
+  const searchSubmitBtn = document.getElementById('search_submit')
+
+  // event listener on enter keystroke from input field
+  searchInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      // get value from input field
+      const searchValue = event.target.value
+
+      // send search input to background.js
+      chrome.runtime.sendMessage({ searchValue })
+    }
+  })
+
+  // add event listener on search submit button
+  searchSubmitBtn.addEventListener('click', (event) => {
+    // get value from input field
+    const searchValue = searchInput.value
+
+    // send search input to background.js
+    chrome.runtime.sendMessage({ searchValue })
+  })
+}
+
+// listen for messages from background.js
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'search_results') {
+    console.log('search results received by popup: ', request)
+
+    const { searchResults: results } = request
+
+    // filter search results from response with similarity score > 0.5
+    const filteredResults = results.filter((result) => {
+      return result.similarity > 0.5
+    })
+
+    // Create new summary component of search results
+    document.getElementById('summaries').innerHTML = ''
+
+    filteredResults.forEach((result) => {
+      const summaryDiv = createSummaryComponent(result, `summary-${result.id}`)
+
+      // Add the summary div to the popup
+      document.getElementById('summaries').appendChild(summaryDiv)
+    })
+
+    // Create view all summaries button with id view_all_summaries
+    const viewAllButton = document.createElement('button')
+    viewAllButton.id = 'view_all_summaries'
+    viewAllButton.innerHTML = 'View All Summaries'
+
+    // append as sibling
+    const searchForm = document.querySelector('.form:has(#search_input)')
+    searchForm.parentNode.insertBefore(viewAllButton, searchForm.nextSibling)
+    viewAllButton.style.display = 'block'
+
+    // event listener on view all summaries button
+    viewAllButton.addEventListener('click', (e) => {
+      document.getElementById('summaries').innerHTML = ''
+      console.log(document.getElementById('summaries'))
+      fetchSummaries()
+
+      // remove view all summaries button
+      viewAllButton.remove()
+
+      // clear search input field
+      document.getElementById('search_input').value = ''
+    })
+  }
+})
+
+initSearchFunction()
+
+// Fetch all the summaries from local storage
 function fetchSummaries() {
   //Retrieve all summaries from local storage
   chrome.storage.local.get(null, (items) => {
     //Iterate through all the summaries
     for (let key in items) {
-      if (key !== 'openai-key') {
+      // if key includes summary
+
+      if (key.includes('summary') && !key.includes('summaryIndex')) {
         console.log(items[key])
+
         // Create a new div to display the summary
-        const summaryDiv = document.createElement('div')
-        summaryDiv.className = 'summary'
-
-        // Create a new div to display the summary text
-        const summaryTextDiv = document.createElement('div')
-        const summaryText = items[key]['content']
-        summaryTextDiv.innerHTML = summaryText
-        summaryTextDiv.className = 'summary-text'
-
-        // Create a new button to copy the summary
-        const copyButton = document.createElement('button')
-        copyButton.innerHTML = clipboardIcon
-        copyButton.className = 'copy-button'
-        copyButton.onclick = function () {
-          //Copy the summary to the clipboard
-          navigator.clipboard.writeText(summaryText)
-          copyButton.innerHTML = clipboardSuccessIcon
-
-          setTimeout(() => {
-            copyButton.innerHTML = clipboardIcon
-          }, 1000)
-        }
-
-        // Create a new button to delete the summary
-        const deleteButton = document.createElement('button')
-        deleteButton.innerHTML = deleteIcon
-        deleteButton.className = 'delete-button'
-        deleteButton.onclick = function () {
-          //Delete the summary from local storage
-          chrome.storage.local.remove(key)
-          // Remove the summary div from the popup
-          summaryDiv.remove()
-        }
-
-        //Add the copy and delete buttons to the summary div
-        summaryDiv.appendChild(summaryTextDiv)
-        summaryDiv.appendChild(copyButton)
-        summaryDiv.appendChild(deleteButton)
+        const summaryDiv = createSummaryComponent(items[key], key)
 
         //Add the summary div to the popup
         document.getElementById('summaries').appendChild(summaryDiv)
-
-        // Add event listener to the summary text div
-        summaryTextDiv.addEventListener('click', (e) => {
-          chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.tabs.sendMessage(
-              tabs[0].id,
-              {
-                type: 'inject',
-                message: items[key],
-              },
-              (response) => {
-                console.log(response)
-              }
-            )
-          })
-        })
       }
     }
   })
 }
 
 fetchSummaries()
+
+function createSummaryComponent(summaryObj, key) {
+  const summaryDiv = document.createElement('div')
+  summaryDiv.className = 'summary'
+
+  // Create a new div to display the summary text
+  const summaryTextDiv = document.createElement('div')
+  const summaryText = summaryObj['content']
+  summaryTextDiv.innerHTML = summaryText
+  summaryTextDiv.className = 'summary-text'
+
+  // Create a new button to copy the summary
+  const copyButton = document.createElement('button')
+  copyButton.innerHTML = clipboardIcon
+  copyButton.className = 'copy-button'
+  copyButton.onclick = function () {
+    //Copy the summary to the clipboard
+    navigator.clipboard.writeText(summaryText)
+    copyButton.innerHTML = clipboardSuccessIcon
+
+    setTimeout(() => {
+      copyButton.innerHTML = clipboardIcon
+    }, 1000)
+  }
+
+  // Create a new button to delete the summary
+  const deleteButton = document.createElement('button')
+  deleteButton.innerHTML = deleteIcon
+  deleteButton.className = 'delete-button'
+  deleteButton.onclick = function () {
+    //Delete the summary from local storage
+    chrome.storage.local.remove(key)
+    // Remove the summary div from the popup
+    summaryDiv.remove()
+  }
+
+  //Add the copy and delete buttons to the summary div
+  summaryDiv.appendChild(summaryTextDiv)
+  summaryDiv.appendChild(copyButton)
+  summaryDiv.appendChild(deleteButton)
+
+  // Add event listener to the summary text div
+  summaryTextDiv.addEventListener('click', (e) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        {
+          type: 'inject',
+          message: summaryObj,
+        },
+        (response) => {
+          console.log(response)
+        }
+      )
+    })
+  })
+  return summaryDiv
+}
--- scripts/background.js
+++ scripts/background.js
@@ -6,6 +6,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // Send a message to the content script to inject the summary
     generateCompletionAction(request.text, request.info, request.tab)
   }
+
+  if (request.searchValue) {
+    searchSummaries(request.searchValue)
+  }
 })
 
 // On extension installation
@@ -18,9 +22,11 @@ chrome.runtime.onInstalled.addListener(() => {
   })
 
   // Create new tab to landing page
-  chrome.tabs.create({
-    url: 'https://fxhd.notion.site/TLDR-Summariser-Chrome-Extension-317ba7f2f1c5443cbc99a220c5d073b0',
-  })
+  // chrome.tabs.create({
+  //   url: 'https://fxhd.notion.site/TLDR-Summariser-Chrome-Extension-317ba7f2f1c5443cbc99a220c5d073b0',
+  // })
+
+  checkSummariesFormat()
 })
 
 // Add listener to commands (keyboard shortcuts)
@@ -57,7 +63,12 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   }
 })
 
-// Utilities
+// On startup
+chrome.runtime.onStartup.addListener(() => {
+  checkSummariesFormat()
+})
+
+// functions
 async function getCurrentTab() {
   let queryOptions = { active: true, lastFocusedWindow: true }
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
@@ -107,7 +118,7 @@ async function generate(prompt) {
     body: JSON.stringify({
       model: 'text-davinci-003',
       prompt: prompt,
-      max_tokens: 256,
+      max_tokens: 1000,
       temperature: 0,
     }),
   })
@@ -133,29 +144,42 @@ async function generateCompletionAction(text, info, tab) {
     sendInjectionMessage({ content: 'generating...' }, tab)
 
     const summaryCompletion = await generate(
-      `Article:\n${text}\n\nArticle summary followed by actionable advice or wisdom in bullet points:`
+      `${text}\n\nDetailed summary of article. Followed by any actionable advice/wisdom in bullet points:
+`
     )
-
     console.log(summaryCompletion)
 
     // Save new summary to local storage
-    const summaryObject = {
+    let summaryObject = {
+      date: Date.now(),
       title: tab.title,
       url: tab.url,
       content: summaryCompletion.text,
     }
 
-    chrome.storage.local.set(
-      {
-        [`summary${Date.now()}`]: summaryObject,
-      },
-      function () {
-        console.log('Summary saved to local storage')
-      }
-    )
-
     // Send the output when we're all done
     sendInjectionMessage(summaryObject, tab)
+
+    // get embedding of summary
+    const summaryEmbedding = await getEmbeddings(
+      `${summaryCompletion.text} \n\n ${summaryObject.title} \n\n ${summaryObject.url}`
+    )
+
+    let id = await getNextId()
+
+    console.log(`this summary's id is: `, id)
+
+    summaryObject = { ...summaryObject, id: id, embedding: summaryEmbedding }
+
+    // save summary to local storage
+    chrome.storage.local.set(
+      {
+        [`summary-${id}`]: summaryObject,
+      },
+      function () {
+        console.log('Summary saved to local storage', summaryObject)
+      }
+    )
   } catch (error) {
     console.log(error)
 
@@ -165,10 +189,172 @@ async function generateCompletionAction(text, info, tab) {
       sendInjectionMessage(
         {
           content:
-            '😢 Your selected text is too long...\nPls select a smaller section and try again!',
+            '😢 your selected text is too long...\npls select a smaller section and try again!',
         },
         tab
       )
     }
   }
+}
+
+async function getNextId() {
+  let result = await new Promise((resolve) =>
+    chrome.storage.local.get(['summaryIndex'], resolve)
+  )
+  let id = result['summaryIndex'] ? result['summaryIndex'] + 1 : 1
+
+  await new Promise((resolve) =>
+    chrome.storage.local.set({ ['summaryIndex']: id }, resolve)
+  )
+  return id
+}
+
+async function getEmbeddings(input) {
+  // Get your API key from storage
+  const key = await getKey()
+  const url = 'https://api.openai.com/v1/embeddings'
+  const model = 'text-embedding-ada-002'
+
+  // Call embeddings endpoint
+  const embeddingsResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: model,
+      input: input,
+    }),
+  })
+
+  const embeddings = await embeddingsResponse.json()
+
+  console.log('embeddings response: ', embeddings)
+
+  if (embeddings.error) {
+    // throw error message
+    throw embeddings.error.message
+  }
+
+  return embeddings.data[0].embedding
+}
+
+async function searchSummaries(query) {
+  try {
+    const searchInputEmbedding = await getEmbeddings(query)
+
+    let searchQueryObject = {
+      date: Date.now(),
+      query: query,
+      embedding: searchInputEmbedding,
+    }
+
+    // Calculate similarity between query and all summaries
+    chrome.storage.local.get(null, (items) => {
+      let searchResults = []
+
+      for (const key in items) {
+        if (key.includes('summary') && !key.includes('summaryIndex')) {
+          const summary = items[key]
+
+          console.log(summary)
+          const similarity = cosineSimilarity(
+            searchInputEmbedding,
+            summary.embedding
+          )
+          searchResults.push({ ...summary, similarity })
+        }
+      }
+
+      // Sort search results by similarity
+      searchResults.sort((a, b) => b.similarity - a.similarity)
+
+      // Store query and its embedding in local storage
+      searchQueryObject = { ...searchQueryObject, results: searchResults }
+
+      chrome.storage.local.set(
+        {
+          [`searchQuery-${Date.now()}`]: searchQueryObject,
+        },
+        function () {
+          console.log(
+            'Search query and results saved to local storage',
+            searchQueryObject
+          )
+        }
+      )
+      console.log(searchResults)
+
+      // Send search results back to popup
+      chrome.runtime.sendMessage({
+        type: 'search_results',
+        searchResults: searchResults,
+      })
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function cosineSimilarity(vecA, vecB) {
+  if (!vecA || !vecB) {
+    console.log('vecA: ', vecA)
+    console.log('vecB: ', vecB)
+    throw new Error('Input vectors are not defined')
+  }
+
+  const dotProduct = (vecA, vecB) => {
+    let product = 0
+    for (let i = 0; i < vecA.length; i++) {
+      product += vecA[i] * vecB[i]
+    }
+    return product
+  }
+
+  const magnitude = (vec) => {
+    let sum = 0
+    for (let i = 0; i < vec.length; i++) {
+      sum += vec[i] * vec[i]
+    }
+    return Math.sqrt(sum)
+  }
+
+  return dotProduct(vecA, vecB) / (magnitude(vecA) * magnitude(vecB))
+}
+
+function checkSummariesFormat() {
+  chrome.storage.local.get(null, async (items) => {
+    for (const key in items) {
+      if (key.includes('summary') && !key.includes('summaryIndex')) {
+        const summary = items[key]
+
+        // check if summary contains embedding and id property
+        if (!summary.embedding || !summary.id) {
+          console.log('summary missing embedding or id', summary)
+
+          const id = await getNextId()
+
+          // create new summary object with id and embedding
+          const newSummary = {
+            ...summary,
+            id: id,
+            embedding: getEmbeddings(
+              `${summary.content} \n\n ${summary.title} \n\n ${summary.url} `
+            ),
+          }
+
+          // delete old summary object from local storage
+          chrome.storage.local.remove(key)
+
+          // save new summary object to local storage
+          chrome.storage.local.set({
+            [`summary-${newSummary.id}`]: newSummary,
+          })
+        }
+      }
+    }
+
+    console.log(items)
+  })
 }
